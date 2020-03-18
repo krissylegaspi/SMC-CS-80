107bb5e2fad699aa23afbe70bb19f010d3829eaa
Kristine Legaspi <krissylegaspi@gmail.com>
2020-03-18 13:03:47 -0700

    Add files via upload

---
+++ Legaspi-Kristine-HelloVB/Legaspi-Kristine-HelloVB/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Legaspi_Kristine_HelloVB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.IblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(132, 58);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(143, 59);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IblMsg
+            // 
+            this.IblMsg.AutoSize = true;
+            this.IblMsg.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.IblMsg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.IblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IblMsg.Location = new System.Drawing.Point(132, 150);
+            this.IblMsg.Name = "IblMsg";
+            this.IblMsg.Size = new System.Drawing.Size(47, 22);
+            this.IblMsg.TabIndex = 1;
+            this.IblMsg.Text = "XXX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 248);
+            this.Controls.Add(this.IblMsg);
+            this.Controls.Add(this.btnDisplay);
+            this.Name = "Form1";
+            this.Text = "Kristine Legaspi - Assignment - VB Introduction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label IblMsg;
+    }
+}
+
